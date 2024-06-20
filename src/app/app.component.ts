a3bb52c0c75f825562ba87f4b98d19d27918da4c
Mukhammadamin <116802751+Mukhammad-amin-ai@users.noreply.github.com>
2024-06-20 16:23:32 +0500

    my first angular app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet, RouterLink, RouterLinkActive} from '@angular/router';
+import {NgOptimizedImage} from "@angular/common";
+import {HeaderComponent} from "./header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, NgOptimizedImage, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'my-app';
+  backgroundImg1 = "assets/background/bg1.svg"
+  backgroundImg2 = "assets/background/bg2.png"
+  hero = "assets/hero.svg"
+  title = "my-app"
+  protected readonly console = console;
+  protected readonly JSON = JSON;
 }
